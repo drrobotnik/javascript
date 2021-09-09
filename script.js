34e69b08dccc9903b5d7b82a6d50f687dc352ba2
Brandon Lavigne <brandon.lavigne@thermofisher.com>
2021-09-08 20:25:29 -0700

    live date time

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 
 
-function formatDate(dateObject) {
-
-    const parts = {
-        date: dateObject.getDate(),
-        month: dateObject.getMonth() + 1,
-        year: dateObject.getFullYear(),
-        hour: (dateObject.getHours() % 12) || 12,
-        minute: dateObject.getMinutes().toString().padStart(2, "0"),
-        amOrPm: dateObject.getHours() < 12 ? "AM" : "PM"
-    };
-
-    document.getElementById("showDate").innerHTML = `${parts.date}.${parts.month}.${parts.year} ${parts.hour}:${parts.minute} ${parts.amOrPm}`;
-
-    // return `${parts.date}.${parts.month}.${parts.year} ${parts.hour}:${parts.minute} ${parts.amOrPm}`;
+function formatDate() {
+    const dateNode = document.getElementById('showDate');
+    setInterval(() => {
+        const date = new Date();
+        dateNode.textContent = date.toDateString() + ' ' + date.toLocaleTimeString('en-US');}, 1000);
 }
 
-const myDate = new Date();
-const myDateFormatted = formatDate(myDate);
+
+formatDate();

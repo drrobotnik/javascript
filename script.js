7493879268fe0cc7e87e3b7630cd7aee3a254afc
NoDisassemble <anything@dant35an0nym0u5.com>
2021-10-10 11:03:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+// Load Landing Page
+function initialLoad() {
+    xhttp = new XMLHttpRequest();
+    xhttp.open("GET", 'templates/landing.html');
+    xhttp.send();
+    xhttp.onload = function () { template.innerHTML = xhttp.responseText }
+};
+
+initialLoad();
+
+// Set active class
+// var header = document.getElementById("menu");
+// var links = header.getElementsByClassName("links");
+// for (var i = 0; i < links.length; i++) {
+//     links[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// };
+
+// Load external HTML files
+function loadHtml(id, filename) {
+    console.log(`div id: ${id}, filename: ${filename}`);
+
+    let xhttp;
+    let element = document.getElementById(id);
+    let file = filename;
+
+    if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) { element.innerHTML = this.responseText; }
+                if (this.status == 404) { element.innerHTML = "<h1>Page not found</h1>"; }
+            }
+        }
+        xhttp.open("GET", `templates/${file}`, true);
+        xhttp.send();
+        return;
+    }
+}
+
+
+
 /* Show time */
 function formatDate() {
     const dateNode = document.getElementById('showDate');

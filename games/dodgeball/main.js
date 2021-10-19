817b7af1c04c69df0c503c9fc579ee539fe51a22
NoDisassemble <anything@dant35an0nym0u5.com>
2021-10-18 22:05:07 -0700

    Add files via upload

---
+++ games/dodgeball/main.js
@@ -0,0 +1,93 @@
+
+function createDivs() {
+    for (let div = 0; div < 98; div++) {
+        cell = document.createElement("div");
+        document.querySelector(".grid").appendChild(cell);
+        cell.classList.add("cell");
+    }
+}
+createDivs();
+
+
+const cells = Array.from(document.querySelectorAll(".cell"));
+const enemyCells = cells.slice(0, 85);
+const playerCells = cells.slice(85);
+const scoreDisplay = document.querySelector(".score");
+
+
+let dropCount, speed, score;
+
+reset();
+
+document.addEventListener("keydown", e => {
+    if (!dropCount) {
+        startGame();
+    }
+
+    const player = document.querySelector(".player");
+
+    if (e.key === "ArrowRight" && playerCells.includes(player.parentElement.nextElementSibling)) {
+        player.parentElement.nextElementSibling.appendChild(player);
+    }
+
+    if (e.key === "ArrowLeft" && playerCells.includes(player.parentElement.previousElementSibling)) {
+        player.parentElement.previousElementSibling.appendChild(player);
+    }
+});
+
+function reset() {
+    dropCount = 0;
+    speed = 1000;
+    score = 0;
+    scoreDisplay.innerHTML = "0";
+
+    cells.forEach(cell => cell.innerHTML = "");
+    playerCells[6].innerHTML = '<div class="player"></div>';
+}
+
+function startGame() {
+    reset();
+    loop();
+}
+
+function loop() {
+    let stopGame = false;
+
+    for (let i = enemyCells.length - 1; i >= 0; i--) {
+        const cell = enemyCells[i];
+        const nextCell = cells[i + 13];
+        const enemy = cell.children[0];
+
+        if (!enemy) {
+            continue;
+        }
+
+        nextCell.appendChild(enemy);
+
+        if (playerCells.includes(nextCell)) {
+            if (nextCell.querySelector(".player")) {
+                stopGame = true;
+            } else {
+                score++;
+                speed = Math.max(100, speed - 25);
+                scoreDisplay.innerHTML = score;
+                enemy.remove();
+            }
+        }
+    }
+
+    // Even drop count, add new enemy
+    if (dropCount % 2 === 0) {
+        const position = Math.floor(Math.random() * 3);
+
+        enemyCells[position].innerHTML = '<div class="enemy"></div>';
+    }
+
+    if (stopGame) {
+        alert('Your score: ' + score + ". Click OK to play again.");
+        reset();
+    } else {
+        dropCount++;
+        setTimeout(loop, speed);
+    }
+}

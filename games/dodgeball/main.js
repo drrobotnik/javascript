fc28f9b407fbf864bb00f04b8b09ba9e2c80e997
NoDisassemble <anything@dant35an0nym0u5.com>
2021-10-20 23:33:33 -0700

    Add files via upload

--- games/dodgeball/main.js
+++ games/dodgeball/main.js
@@ -1,12 +1,12 @@
 
-function createDivs() {
+function createGrid() {
     for (let div = 0; div < 54; div++) {
         cell = document.createElement("div");
         document.querySelector(".grid").appendChild(cell);
         cell.classList.add("cell");
     }
 }
-createDivs();
+createGrid();
 
 
 const cells = Array.from(document.querySelectorAll(".cell"));
@@ -14,11 +14,36 @@ const enemyCells = cells.slice(0, 45);
 const playerCells = cells.slice(45);
 const scoreDisplay = document.querySelector(".score");
 
-
 let dropCount, speed, score;
+
+let throwFX = new Audio("./assets/throw.mp3");
+let whistleStop = new Audio("./assets/whistlestop.mp3");
+let yourout = new Audio("./assets/yourout.mp3");
 
 reset();
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+
+let bgAudio = new Audio('./assets/ifyoucandodgeawrench.mp3');
+if (typeof bgAudio.loop == 'boolean') {
+    bgAudio.loop = true;
+}
+else {
+    bgAudio.addEventListener('ended', function () {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+}
+
+
+// start game after any key is pressed
 document.addEventListener("keydown", e => {
     if (!dropCount) {
         startGame();
@@ -47,6 +72,8 @@ function reset() {
 
 function startGame() {
     reset();
+    bgAudio.play();
+    sleep(8000);
     loop();
 }
 
@@ -56,8 +83,8 @@ function loop() {
     for (let i = enemyCells.length - 1; i >= 0; i--) {
         const cell = enemyCells[i];
         const nextCell = cells[i + 9];
+        throwFX.play();
         const enemy = cell.children[0];
-
         if (!enemy) {
             continue;
         }
@@ -67,9 +94,11 @@ function loop() {
         if (playerCells.includes(nextCell)) {
             if (nextCell.querySelector(".player")) {
                 stopGame = true;
+                yourout.play();
+
             } else {
                 score++;
-                speed = Math.max(100, speed - 25);
+                speed = Math.max(100, speed - 15);
                 scoreDisplay.innerHTML = score;
                 enemy.remove();
             }
@@ -78,14 +107,16 @@ function loop() {
 
     // Even drop count, add new enemy
     if (dropCount % 2 === 0) {
-        const position = Math.floor(Math.random() * 3);
+        const position = Math.floor(Math.random() * 9);
 
         enemyCells[position].innerHTML = '<div class="enemy"></div>';
     }
 
     if (stopGame) {
+        bgAudio.pause();
         alert('Your score: ' + score + ". Click OK to play again.");
-        reset();
+        location.reload();
+        // reset();
     } else {
         dropCount++;
         setTimeout(loop, speed);
